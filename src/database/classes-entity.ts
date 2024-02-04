aa2aff2f27da9cd4da159a5c9907cd7a574bee27
Elayade Ismail <elayadeismail@gmail.com>
2024-02-04 14:04:04 +0100

    Add Create ClassEntity To DB

---
+++ src/database/classes-entity.ts
@@ -0,0 +1,58 @@
+import { InsertClassSchemaType } from "@/lib/validators/classes-entity";
+import prisma from "./db";
+import { getErrorMessage } from "@/lib/utils";
+
+export const createClass = async (inputs: InsertClassSchemaType) => {
+  try {
+    const level = await prisma.level.findUnique({
+      where: {
+        id: inputs.levelId
+      }
+    })
+    if (!level) return { status: "error", message: "level does not exist" } as const
+    let className = level.name
+    if (inputs.subjects.length > 1) {
+      const subjects = await prisma.subject.findMany({
+        where: {
+          id: { in: inputs.subjects }
+        },
+        select: {
+          name: true
+        }
+      })
+      const subNames = subjects.map(({ name }) => name).join(", ")
+      className = `${className}, ${subNames}`
+    }
+    if (inputs.majors.length > 1) {
+      const majors = await prisma.majorStudent.findMany({
+        where: {
+          id: { in: inputs.majors }
+        },
+        select: {
+          name: true
+        }
+      })
+      const majNames = majors.map(({ name }) => name).join(", ")
+      className = `${className}, ${majNames}`
+    }
+    const recored = await prisma.classEntity.create({
+      data: {
+        name: className,
+        levelId: inputs.levelId,
+        price: inputs.price,
+        subjects: {
+          connect: inputs.subjects.map((id) => ({ id }))
+        },
+        majors: {
+          connect: inputs.majors.map((id) => ({ id }))
+        }
+      },
+      select: {
+        id: true
+      }
+    })
+    return { status: "success", data: recored } as const
+  } catch (err) {
+    return { status: "error", message: getErrorMessage(err) } as const
+  }
+}
